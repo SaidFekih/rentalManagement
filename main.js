c8bd9aab44cbeafde65e3dd3beb9e56a2b597893
Said Fekih <58040633+SaidFekih@users.noreply.github.com>
2022-03-06 19:36:28 -0500

    Add header

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ var app = new Vue({
     el:"#app",
     data:{
         user:"Ines",
-        
+        clients: [
+            { unitNumber:'1',firstName: 'Mohamed', lastName: 'Salah', phoneNumber:5144444444, email: 'Mohamde.salah@test.com', endDate: '20 fev' },
+            { unitNumber:'1',firstName: 'Mohamed', lastName: 'Salah', phoneNumber:5144444444, email: 'Mohamde.salah@test.com', endDate: '20 fev' },
+            { unitNumber:'1',firstName: 'Mohamed', lastName: 'Salah', phoneNumber:5144444444, email: 'Mohamde.salah@test.com', endDate: '20 fev' },
+            { unitNumber:'1',firstName: 'Mohamed', lastName: 'Salah', phoneNumber:5144444444, email: 'Mohamde.salah@test.com', endDate: '20 fev' },
+            { unitNumber:'1',firstName: 'Mohamed', lastName: 'Salah', phoneNumber:5144444444, email: 'Mohamde.salah@test.com', endDate: '20 fev' },
+        ]
 
     }
 })
